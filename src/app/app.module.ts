17f91a2d00c2c45ef0dd006cdbd5c3347d612be8
UcheSylvester <okorocode@gmail.com>
2019-09-22 20:31:43 +0100

    creating event service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,19 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { EventsListComponent } from "./events/events-list.component";
-import { EventThumbnailComponent } from './events/event-thumbnail/event-thumbnail.component';
-import { NavComponent } from './nav/nav.component';
+import { EventThumbnailComponent } from "./events/event-thumbnail/event-thumbnail.component";
+import { NavComponent } from "./nav/nav.component";
+import { EventService } from "./shared/events.services";
 
 @NgModule({
-  declarations: [AppComponent, EventsListComponent, EventThumbnailComponent, NavComponent],
+  declarations: [
+    AppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavComponent
+  ],
   imports: [BrowserModule],
-  providers: [],
+  providers: [EventService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
