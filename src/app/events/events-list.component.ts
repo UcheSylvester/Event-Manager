c639f8e5e97c4b07787fb34bcbad8b67ccbf5e4d
UcheSylvester <okorocode@gmail.com>
2019-09-22 11:01:58 +0100

    styling our component

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-events-list",
-  templateUrl: "./events-list.component.html"
+  templateUrl: "./events-list.component.html",
+  styleUrls: ['./events-list.component.css']
 })
 export class EventsListComponent {
   events = [
