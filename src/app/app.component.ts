1f5387ef8f885abb62a9b86a1528892c0154c033
UcheSylvester <okorocode@gmail.com>
2019-09-22 10:46:31 +0100

    creating nav-bar, hard-coding events for now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-root",
   template: `
+    <app-nav></app-nav>
     <app-events-list></app-events-list>
   `
   // templateUrl: "./app.component.html"
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { EventsListComponent } from "./events/events-list.component";
 import { EventThumbnailComponent } from './events/event-thumbnail/event-thumbnail.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [AppComponent, EventsListComponent, EventThumbnailComponent],
+  declarations: [AppComponent, EventsListComponent, EventThumbnailComponent, NavComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
